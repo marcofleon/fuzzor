3081204370a2533fb15daedf1c678f6f87420a5c
fanquake <fanquake@gmail.com>
2024-08-13 12:38:54 +0100

    infra: parameterize GCC version
    Also allows building like: `docker build . --build-arg GCC_V=13`, if you
    want to test using a different GCC (that is available in the distro).
    
    Also make sure we are calling the same version of AR. Using a version
    different from the compiler / linker etc can sometimes lead to subtle
    issues or broken builds.

--- projects/openssl/build.sh
+++ projects/openssl/build.sh
@@ -8,9 +8,9 @@ rm -rf ./*
 git checkout .
 
 if [[ $FUZZING_ENGINE =~ semsan_Custom0 ]]; then
-  CC=gcc-14 CXX=g++-14 LD=gcc-14 AR=ar ./config linux-armv4 enable-md2 enable-rc5 --cross-compile-prefix=arm-linux-gnueabihf-
+  CC=gcc-${GCC_V} CXX=g++-${GCC_V} LD=gcc-${GCC_V} AR=gcc-ar-${GCC_V} ./config linux-armv4 enable-md2 enable-rc5 --cross-compile-prefix=arm-linux-gnueabihf-
 elif [[ $FUZZING_ENGINE =~ semsan_Custom1 ]]; then
-  CC=gcc-14 CXX=g++-14 LD=gcc-14 AR=ar ./config linux-x86_64 enable-md2 enable-rc5 --cross-compile-prefix=x86_64-linux-gnu-
+  CC=gcc-${GCC_V} CXX=g++-${GCC_V} LD=gcc-${GCC_V} AR=gcc-ar-${GCC_V} ./config linux-x86_64 enable-md2 enable-rc5 --cross-compile-prefix=x86_64-linux-gnu-
 else
   ./config enable-md2 enable-rc5
 fi
@@ -32,16 +32,16 @@ git apply ../gcc.patch # Allows us to build cryptofuzz with gcc
 
 # Force cpu_features to compile for the right architecure
 if [[ $FUZZING_ENGINE =~ semsan_Custom0 ]]; then
-  export CC=arm-linux-gnueabihf-gcc-14
-  export CXX=arm-linux-gnueabihf-g++-14
-  export LD=arm-linux-gnueabihf-gcc-14
-  export AR=arm-linux-gnueabihf-ar
+  export CC=arm-linux-gnueabihf-gcc-${GCC_V}
+  export CXX=arm-linux-gnueabihf-g++-${GCC_V}
+  export LD=arm-linux-gnueabihf-gcc-${GCC_V}
+  export AR=arm-linux-gnueabihf-gcc-ar-${GCC_V}
   git apply ../cpu_feartues_arm32.patch
 elif [[ $FUZZING_ENGINE =~ semsan_Custom1 ]]; then
-  export CC=x86_64-linux-gnu-gcc-14
-  export CXX=x86_64-linux-gnu-g++-14
-  export LD=x86_64-linux-gnu-gcc-14
-  export AR=x86_64-linux-gnu-ar
+  export CC=x86_64-linux-gnu-gcc-${GCC_V}
+  export CXX=x86_64-linux-gnu-g++-${GCC_V}
+  export LD=x86_64-linux-gnu-gcc-${GCC_V}
+  export AR=x86_64-linux-gnu-gcc-ar-${GCC_V}
   git apply ../cpu_feartues_x86_64.patch
 fi
 
--- projects/secp256k1/build.sh
+++ projects/secp256k1/build.sh
@@ -3,16 +3,16 @@
 set -ex
 
 if [[ $FUZZING_ENGINE =~ semsan_Custom[0-1] ]]; then
-  export CC=arm-linux-gnueabihf-gcc-14
-  export CXX=arm-linux-gnueabihf-g++-14
-  export LD=arm-linux-gnueabihf-gcc-14
-  export AR=arm-linux-gnueabihf-ar
+  export CC=arm-linux-gnueabihf-gcc-${GCC_V}
+  export CXX=arm-linux-gnueabihf-g++-${GCC_V}
+  export LD=arm-linux-gnueabihf-gcc-${GCC_V}
+  export AR=arm-linux-gnueabihf-gcc-ar-${GCC_V}
   export CMAKE_TOOLCHAIN_FILE=/toolchains/arm32.cmake
 elif [[ $FUZZING_ENGINE =~ semsan_Custom[2-3] ]]; then
-  export CC=x86_64-linux-gnu-gcc-14
-  export CXX=x86_64-linux-gnu-g++-14
-  export LD=x86_64-linux-gnu-gcc-14
-  export AR=x86_64-linux-gnu-ar
+  export CC=x86_64-linux-gnu-gcc-${GCC_V}
+  export CXX=x86_64-linux-gnu-g++-${GCC_V}
+  export LD=x86_64-linux-gnu-gcc-${GCC_V}
+  export AR=x86_64-linux-gnu-gcc-ar-${GCC_V}
   export CMAKE_TOOLCHAIN_FILE=/toolchains/x86_64.cmake
 fi
 
