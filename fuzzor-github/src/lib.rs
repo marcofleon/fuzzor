cf354b6e1b791c1a3e197c1640cd970c727334d7
dergoegge <n.goeggi@gmail.com>
2025-03-26 13:59:21 +0000

    cargo fmt

--- fuzzor-github/src/lib.rs
+++ fuzzor-github/src/lib.rs
@@ -1,2 +1,2 @@
-pub mod revisions;
 pub mod reporter;
+pub mod revisions;
--- fuzzor/src/lib.rs
+++ fuzzor/src/lib.rs
@@ -3,5 +3,5 @@
 pub mod corpora;
 pub mod env;
 pub mod project;
-pub mod solutions;
 pub mod revisions;
+pub mod solutions;
